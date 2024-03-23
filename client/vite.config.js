8a6a8f194ae5ed141e676ec12bacb1510fd796ea
pmyyyy <promp@LAPTOP-69L9RNF3>
2024-03-23 17:44:05 +0700

    school management system

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,14 +8,3 @@ export default defineConfig({
     'process.env': process.env,
   },
 })
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-})
