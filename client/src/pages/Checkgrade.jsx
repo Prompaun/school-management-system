75ce97a905da5bb46273482402190848ff806179
pmyyyy <promp@LAPTOP-69L9RNF3>
2024-03-24 19:53:11 +0700

    modiify grade system ui

--- client/src/pages/Checkgrade.jsx
+++ client/src/pages/Checkgrade.jsx
@@ -299,103 +299,114 @@ useEffect(() => {
       <Header header="ระบบจัดการสารสนเทศ" subhead="บริการสำหรับนักเรียน" />
       <br />
       <div className="d-flex flex-column"style={{fontFamily: 'Kanit, sans-serif',height:"100vh"}}>
-      <div className="container d-flex flex-column align-items-center justify-content-center" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-        <h2>ระบบตรวจสอบผลการเรียน</h2><br />
-      </div>
-
-      <div className="container d-flex align-items-center justify-content-center"style={{ flexWrap: 'wrap' }}>
-        <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px', fontSize: '18px'}}>
-            <div className="d-flex align-items-center">
-              <span style={{marginRight:"10px"}}>ปีการศึกษา :</span>
-            </div>
-            <div className="dropdown" style={{ maxWidth: '100%' }}>
-              <select value={selectedYear} onChange={handleYearChange} className="custom-select">
-                <option value="">เลือกปีการศึกษา</option>
-                {Year.map((year) => (
-                  <option key={year} value={year}>
-                    {year}
-                  </option>
-                ))}
-              </select>
-            </div>
+        <div className="container d-flex flex-column align-items-center justify-content-center" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+          <h2>ระบบตรวจสอบผลการเรียน</h2><br />
         </div>
-         
-        <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px', fontSize: '18px'}}>
-          <div className="d-flex align-items-center">
-            <span style={{marginRight:"10px"}}>ภาคเรียนที่ :</span>
-          </div>
-          
 
-          <div className="dropdown" style={{ maxWidth: '100%' }}>
-                <select value={selectedSemester} onChange={handleSemesterChange} className="custom-select">
-                  <option value="">เลือกภาคเรียน</option>
-                  {Semester.map((semesterData) => (
-                        <option key={semesterData} value={semesterData}>
-                          {semesterData}
-                        </option>
-                      ))}
+        <div className="container d-flex align-items-center justify-content-center"style={{ flexWrap: 'wrap' }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px', fontSize: '18px'}}>
+
+              <div className="d-flex align-items-center">
+                <span style={{marginRight:"10px"}}>ปีการศึกษา :</span>
+              </div>
+
+              <div className="dropdown" style={{ maxWidth: '100%' }}>
+                <select value={selectedYear} onChange={handleYearChange} className="custom-select">
+                  <option value="">เลือกปีการศึกษา</option>
+                  {Year.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
                 </select>
               </div>
+          </div>
+          
+          <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px', fontSize: '18px'}}>
+            <div className="d-flex align-items-center">
+              <span style={{marginRight:"10px"}}>ภาคเรียนที่ :</span>
+            </div>
+
+            <div className="dropdown" style={{ maxWidth: '100%' }}>
+              <select value={selectedSemester} onChange={handleSemesterChange} className="custom-select">
+                <option value="">เลือกภาคเรียน</option>
+                {Semester.map((semesterData) => (
+                      <option key={semesterData} value={semesterData}>
+                        {semesterData}
+                      </option>
+                    ))}
+              </select>
+            </div>
+          </div>
         </div>
+        <br /> {/* เพิ่มแท็ก <br /> เพื่อสร้างการเว้นบรรทัด margin: 'auto', */}
+
+      {subjectObject.length === 0 ? (
+        <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px', fontSize: '18px' }}>
+            <div className="container mt-5 d-flex flex-column align-items-center">
+                <span className="ms-3 mb-0" style={{ color: 'gray' }}>ไม่พบข้อมูลเกรดของท่าน</span>
+            </div>
+        </div>
+      ) : (
+        <>
+          
+          <div className="container flex-column align-items-center">
+          {/* <table className="table-bordered" style={{ textAlign: 'center',fontFamily: 'Kanit, sans-serif'}}> */}
+            <div className="d-flex justify-content-center" style={{ height: 'auto', overflowY: 'auto' }}>
+              <div className="table-responsive">
+                <table className="table table-bordered table-striped table-hover" style={{ borderCollapse: 'collapse', textAlign: 'center',fontFamily: 'Kanit, sans-serif' }}>
+                  <thead>          
+                    <tr>
+                      <th colSpan="9" style={{ textAlign: 'center' }}>{tableHeader}</th>
+                    </tr>
+
+                    <tr>
+                      <th rowSpan="3">รหัสวิชา</th>
+                      <th rowSpan="3">ชื่อวิชา</th>
+                      <th rowSpan="3">หน่วยกิต</th>
+                      <th colSpan="4" style={{ textAlign: 'center' }}>ผลการเรียน</th>
+                      <th rowSpan="3">คะแนนรวม</th>
+                      <th rowSpan="3">เกรด</th>
+                    </tr>
+
+                    <tr>
+                      <th colSpan="2">ระหว่างภาค</th>
+                      <th colSpan="2">ปลายภาค</th>
+                    </tr>
+
+                    <tr>
+                      <th>เต็ม</th>
+                      <th>ได้</th>
+                      <th>เต็ม</th>
+                      <th>ได้</th>
+                    </tr>
+
+                  </thead>
+
+                  <tbody>
+                    {subjectObject.map((subject) => (
+                      <tr key={subject.id} style={{ height: '50px' }}>
+                        <td >{subject.id}</td>
+                        <td >{subject.name}</td>
+                        <td >{subject.credits}</td>
+                        <td >{subject.between_full}</td>
+                        <td >{subject.between_get}</td>
+                        <td >{subject.final_full}</td>
+                        <td >{subject.final_get}</td>
+                        <td >{subject.totalScore}</td>
+                        <td >{subject.grade}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+
+                </table>
+              <br />
+              </div>
+            </div> 
+          </div>
+        </>
+      )}
       </div>
-      <br /> {/* เพิ่มแท็ก <br /> เพื่อสร้างการเว้นบรรทัด margin: 'auto', */}
-        
-        <div className="container flex-column align-items-center">
-        {/* <table className="table-bordered" style={{ textAlign: 'center',fontFamily: 'Kanit, sans-serif'}}> */}
-        <div className="d-flex justify-content-center" style={{ height: 'auto', overflowY: 'auto' }}>
-        <div className="table-responsive">
-        <table className="table table-bordered table-striped table-hover" style={{ borderCollapse: 'collapse', textAlign: 'center',fontFamily: 'Kanit, sans-serif' }}>
-
-
-        <thead>          
-          <tr>
-            <th colSpan="9" style={{ textAlign: 'center' }}>{tableHeader}</th>
-          </tr>
-
-          <tr>
-            <th rowSpan="3">รหัสวิชา</th>
-            <th rowSpan="3">ชื่อวิชา</th>
-            <th rowSpan="3">หน่วยกิต</th>
-            <th colSpan="4" style={{ textAlign: 'center' }}>ผลการเรียน</th>
-            <th rowSpan="3">คะแนนรวม</th>
-            <th rowSpan="3">เกรด</th>
-          </tr>
-          <tr>
-            <th colSpan="2">ระหว่างภาค</th>
-            <th colSpan="2">ปลายภาค</th>
-          </tr>
-          <tr>
-            <th>เต็ม</th>
-            <th>ได้</th>
-            <th>เต็ม</th>
-            <th>ได้</th>
-          </tr>
-        </thead>
-
-        <tbody>
-      {subjectObject.map((subject) => (
-        <tr key={subject.id} style={{ height: '50px' }}>
-          <td >{subject.id}</td>
-          <td >{subject.name}</td>
-          <td >{subject.credits}</td>
-          <td >{subject.between_full}</td>
-          <td >{subject.between_get}</td>
-          <td >{subject.final_full}</td>
-          <td >{subject.final_get}</td>
-          <td >{subject.totalScore}</td>
-          <td >{subject.grade}</td>
-        </tr>
-      ))}
-    </tbody>
-
-
-        </table>
-        <br />
-        </div>
-        </div>
-        
-        </div>
-        </div>
       {/* </div> */}
     </>
   );
